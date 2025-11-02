8a8885a4d68df013bbb74cbe1de507b3b954e4a2
Maverick <sabatini.m@gmail.com>
2025-11-02 16:57:57 +0100

    Add decode unknown work

--- tests/test_tiktoken.py
+++ tests/test_tiktoken.py
@@ -24,9 +24,17 @@ def test_tiktoken_decode():
 
     assert decoded == "Hello do you like tea?<|endoftext|> In the sunlit terraces of the palace."
 
-def test_uknown_word():
+def test_encode_uknown_word():
     tokenizer = tiktoken.get_encoding("gpt2")
+    
     encoded = tokenizer.encode("Akwirw ier", allowed_special={"<|endoftext|>"})
 
     assert encoded == [33901, 86, 343, 86, 220, 959]
 
+
+def test_decode_uknown_word():
+    tokenizer = tiktoken.get_encoding("gpt2")
+    
+    decoded = tokenizer.decode([33901, 86, 343, 86, 220, 959])
+
+    assert decoded == "Akwirw ier"
